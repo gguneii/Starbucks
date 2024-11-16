5800ebd5baa9750f8a5842c29f4134836e92be6e
gguneii <guneysuleymanofa@gmail.com>
2024-11-16 23:39:13 +0400

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Layout from "./layouts/Layout"
-import Main from "./components/Main/Main"
+import Main from "./components/main/Main"
 
 function App() {
   return (
